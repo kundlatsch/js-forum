5d408ea88de0b311cdb6749b8895310b60b0a63c
kundlatsch <gustavo.kundlatsch@gmail.com>
2021-05-23 17:18:21 -0300

    Add cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const db = require('./models');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const db = require('./models');
 
 const postsRouter = require('./routes/Posts');
 app.use("/posts", postsRouter);
