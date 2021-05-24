d1704e4aadddc69735fc70560d231d28366218c0
kundlatsch <gustavo.kundlatsch@gmail.com>
2021-05-24 01:27:11 -0300

    Create new post page

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import api from '../services/forum-api';
 
 function CreatePost() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: ""
+  }
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required().max(255),
+    postText: Yup.string().required().max(255),
+    username: Yup.string().required().max(255)
+  });
+
+  const onSubmit = (data) => {
+    api.post("/posts", data).then((res) => {
+      console.log('ok')
+    });
+  };
+
+  return (
+    <div className="createPost">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title</label>
+          <ErrorMessage name="title" component="span" />
+          <Field 
+            id="inputCreatePost" 
+            name="title"
+            placeholder="Add Title..."
+          />
+          
+          <ErrorMessage name="postText" component="span" />
+          <Field as="textarea"
+            id="inputPostBody" 
+            name="postText"
+            placeholder=""
+          >
+          </Field>
+
+          <label>Username</label>
+          <ErrorMessage name="username" component="span" />
+          <Field 
+            id="inputCreatePost" 
+            name="username"
+            placeholder="Ex. Taguma..."
+          />
+
+          <button type="submit">Create Post</button>
+
+        </Form>
+      </Formik>
+    </div>
+  )
 }
 
 export default CreatePost
