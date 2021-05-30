8d988a154f810f769b5c21581963b4c49f5293b1
kundlatsch <gustavo.kundlatsch@gmail.com>
2021-05-30 18:13:44 -0300

    Add 404 error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 import { AuthContext } from './helpers/AuthContext';
 import { useState, useEffect } from 'react';
 import api from './services/forum-api';
@@ -74,6 +75,7 @@ function App() {
             <Route path="/post/:id" exact component={Post} />
             <Route path="/register" exact component={Register} />
             <Route path="/login" exact component={Login} />
+            <Route path="*" exact component={PageNotFound} />
           </Switch>
         </Router>
       </AuthContext.Provider>
---
+++ frontend/src/pages/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PageNotFound() {
+  return (
+    <div>
+      <h1 style={{marginTop: "50px"}}>
+          Error 404: Page not found!
+      </h1>
+    </div>
+  )
+}
+
+export default PageNotFound;
